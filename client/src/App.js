a9bde146ad6254acfac7bc209fe6479e6ccf6c45
jrlmasangkay <jrlmasangkay.github@gmail.com>
2022-03-26 10:20:41 +1300

    working frontend

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { get } from "./fetch-helper";
+
+const App = () => {
+  const [products, setProducts] = useState();
+
+  useEffect(async () => {
+    const response = await getProducts();
+    setProducts(response);
+  }, []);
+
+  return (
+    <div>
+      <p>Products</p>
+      {products && products.items.map((product) => <p>{product.name}</p>)}
+    </div>
+  );
+};
+
+const getProducts = async () => {
+  const response = await get(
+    "https://warehousesample.azurewebsites.net/api/products/"
+  );
+
+  return response;
+};
+
+export default App;
