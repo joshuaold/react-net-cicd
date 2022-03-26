e2acfb5e70371869d952f5294b3fd2a0b3936fb9
jrlmasangkay <jrlmasangkay.github@gmail.com>
2022-03-26 14:13:54 +1300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>Updatedsadassad</h1>
+      <h1>fghsfsdsdf</h1>
       {products && products.items.map((product) => <p>{product.name}</p>)}
     </div>
   );
